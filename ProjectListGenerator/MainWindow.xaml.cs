46f5b8988940e6ceaece0c42b90b5c2ba673e5d0
Andreas Ruhrmann <aruhrmann@atisda.de>
2017-08-31 21:28:52 +0200

    Second Commit

--- ProjectListGenerator/MainWindow.xaml.cs
+++ ProjectListGenerator/MainWindow.xaml.cs
@@ -1,18 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Data.SqlClient;
 
 namespace ProjectListGenerator
 {
@@ -24,20 +11,7 @@ namespace ProjectListGenerator
         public MainWindow()
         {
             InitializeComponent();
-            //DBConnector dbconnection = new DBConnector();
-            //dbconnection.Connect("copago1");
-
-            //using (SqlDataReader reader = dbconnection.Query("select * from WarenwirtschaftSet"))
-            //{
-            //    while (reader.Read())
-            //    {
-            //        Console.WriteLine("{0} {1}", reader.GetInt32(0), reader.GetString(1));
-            //    }
-            //}
-
-            //dbconnection.Disconnect();
-            Warenwirtschaft blub = new Warenwirtschaft();
-            Console.WriteLine(blub.GetAll().Count);
+            DataContext = new WarenwirtschaftSet();
         }
     }
 }
--- ProjectListGenerator/Warenwirtschaft.cs
+++ ProjectListGenerator/Warenwirtschaft.cs
@@ -19,31 +19,52 @@ namespace ProjectListGenerator
             this.db = new DBConnector();
             this.db.Connect("copago1");
         }
+        public Warenwirtschaft(int id, string name, DBConnector db)
+        {
+            this.db = db;
+            this.id = id;
+            this.name = name;
+        }
+
         public Warenwirtschaft(int id, string name)
         {
             this.id = id;
             this.name = name;
         }
-
-        public ObservableCollection<Warenwirtschaft> GetAll()
+        public Warenwirtschaft(DBConnector db)
         {
-            ObservableCollection < Warenwirtschaft > returnCollection = new ObservableCollection<Warenwirtschaft>();
-
-            using (SqlDataReader reader = db.Query("select * from WarenwirtschaftSet"))
-            {
-                while (reader.Read())
-                {
-                    Console.WriteLine("DEBUG: {0} {1}", reader.GetInt32(0), reader.GetString(1));
-                    returnCollection.Add(new Warenwirtschaft(reader.GetInt32(0), reader.GetString(1)));
-                }
-            }
-
-            return returnCollection;
+            this.db = db;
         }
+        
     }
 
     class WarenwirtschaftSet
     {
+        private DBConnector db;
 
+        public WarenwirtschaftSet()
+        {
+            this.db = new DBConnector();
+            this.db.Connect("copago1");
+        }
+
+        public ObservableCollection<Warenwirtschaft> All
+        {
+            get
+            {
+                ObservableCollection<Warenwirtschaft> returnCollection = new ObservableCollection<Warenwirtschaft>();
+
+                using (SqlDataReader reader = db.Query("select * from WarenwirtschaftSet"))
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("DEBUG: {0} {1}", reader.GetInt32(0), reader.GetString(1));
+                        returnCollection.Add(new Warenwirtschaft(reader.GetInt32(0), reader.GetString(1), this.db));
+                    }
+                }
+
+                return returnCollection;
+            }
+        }
     }
 }
